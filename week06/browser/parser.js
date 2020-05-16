dcf114670cbf2c8afb41fedd1bcd8f1c5d4e85ab
chon <chon_den@abbieit.com>
2020-05-16 10:00:19 +0800

    handle attribute

--- week06/browser/parser.js
+++ week06/browser/parser.js
@@ -1,6 +1,11 @@
 let state = data;
 let EOF = Symbol('EOF');
 let currentToken = null;
+let currentAttribute = null;
+
+let stack = [{ type: 'document', children: [] }];
+let tokenType = Symbol('type');
+let tag = Symbol('tagName');
 
 module.exports.parseHTML = function parseHTML(html) {
   // console.log(html);
@@ -11,17 +16,17 @@ module.exports.parseHTML = function parseHTML(html) {
 };
 
 function emit(token) {
-  console.log(token);
+  if (token[tokenType] !== 'text') console.log(token);
 }
 
 function data(c) {
   if (c === '<') {
     return tagOpen;
   } else if (c === EOF) {
-    emit({ type: 'EOF' });
+    emit({ [tokenType]: 'EOF' });
     return;
   } else {
-    emit({ type: 'text', content: c });
+    emit({ [tokenType]: 'text', content: c });
   }
   return data;
 }
@@ -31,8 +36,8 @@ function tagOpen(c) {
     return endTagOpen;
   } else if (c.match(/^[a-z]$/i)) {
     currentToken = {
-      type: 'startTag',
-      tagName: '',
+      [tokenType]: 'startTag',
+      [tag]: '',
     };
     return tagName(c);
   } else {
@@ -43,8 +48,8 @@ function tagOpen(c) {
 function endTagOpen(c) {
   if (c.match(/^[a-z]$/i)) {
     currentToken = {
-      type: 'endTag',
-      tagName: '',
+      [tokenType]: 'endTag',
+      [tag]: '',
     };
     return tagName(c);
   } else if (c === '>') {
@@ -60,7 +65,7 @@ function tagName(c) {
     return selfClosingStartTag;
   } else if (c.match(/^[a-z]$/i)) {
     // console.log(c);
-    currentToken.tagName += c;
+    currentToken[tag] += c;
     return tagName;
   } else if (c === '>') {
     emit(currentToken);
@@ -73,18 +78,134 @@ function tagName(c) {
 function beforeAttributeName(c) {
   if (c.match(/^[\t\n\f ]$/)) {
     return beforeAttributeName;
-  } else if (c === '>') {
-    return data;
+  } else if (c === '>' || c === '/' || c === EOF) {
+    return afterAttributeName(c);
   } else if (c === '=') {
-    return beforeAttributeName;
+    throw Error("attribute name can't start with '='");
   } else {
+    currentAttribute = {
+      name: '',
+      value: '',
+    };
+    return attributeName(c);
+  }
+}
+
+function attributeName(c) {
+  if (c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
+    // <input require/>
+    return afterAttributeName(c);
+  } else if (c === '=') {
+    return beforeAttributeValue;
+  } else if (c === '\u0000') {
+  } else if (c === '"' || c === "'" || c === '<') {
+    // This is an unexpected-character-in-attribute-name parse error. Treat it as per the "anything else" entry below.
+    throw Error("attribute name can't includes '" + char + "'");
+  } else {
+    currentAttribute.name += c;
+    // return afterAttributeName;
+    return attributeName;
+  }
+}
+
+function afterAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    // 没有属性值的的属性
+    currentToken[currentAttribute.name] = currentAttribute.value;
+  } else if (c === '/') {
+    // 没有属性值的的属性
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return selfClosingStartTag;
+  } else if (c === '=') {
+    return beforeAttributeValue;
+  } else if (c === '>') {
+    emit(currentToken);
+    return data;
+  } else if (c === EOF) {
+    emit({ [tokenType]: 'EOF' });
+  } else {
+    return attributeName(c);
+  }
+}
+
+function beforeAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
+    // <input type= > TODO
     return beforeAttributeName;
+  } else if (c === '"') {
+    return doubleQuotedAttributeValue;
+  } else if (c === "'") {
+    return signleQuotedAttributeValue;
+  } else if (c === '>') {
+  } else {
+    return unQuotedAttributeValue(c);
+  }
+}
+
+function doubleQuotedAttributeValue(c) {
+  if (c === '"') {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterQuotedAttributeValue;
+  } else if (c === '\u0000') {
+  } else if (c === EOF) {
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
+}
+
+function signleQuotedAttributeValue(c) {
+  if (c === "'") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterQuotedAttributeValue;
+  } else if (c === '\u0000') {
+  } else if (c === EOF) {
+  } else {
+    currentAttribute.value += c;
+    return signleQuotedAttributeValue;
+  }
+}
+
+function afterQuotedAttributeValue(c) {
+  if (c.match(/^[\n\t\f ]$/)) {
+    return beforeAttributeName;
+  } else if (c === '/') {
+    return selfClosingStartTag;
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c === EOF) {
+  } else {
+    // TODO check
+    beforeAttributeName(c);
+  }
+}
+
+function unQuotedAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return beforeAttributeName;
+  } else if (c === '/') {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return selfClosingStartTag;
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c === '\u0000') {
+  } else if (c === "'" || c === '"' || c === '<' || c === '=' || c === '`') {
+  } else if (c === EOF) {
+  } else {
+    currentAttribute.value += c;
+    return unQuotedAttributeValue;
   }
 }
 
 function selfClosingStartTag(c) {
   if (c === '>') {
     currentToken.isSelfClosing = true;
+    emit(currentToken);
     return data;
   } else if (c === EOF) {
   }
--- week06/browser/server.js
+++ week06/browser/server.js
@@ -7,27 +7,29 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('X-Foo', 'bar');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-
+  // <input type="text" required>
   const body = `<html maaa=a >
-  <head>
-      <style>
-  body div #myid{
-      width:100px;
-      background-color: #ff5000;
-  }
-  body div img{
-      width:30px;
-      background-color: #ff1111;
-  }
-      </style>
-  </head>
-  <body>
-      <div>
-          <img id="myid"/>
-          <img />
-      </div>
-  </body>
-  </html>`;
+    <head>
+        <style>
+    body div #myid{
+        width:100px;
+        background-color: #ff5000;
+    }
+    body div img{
+        width:30px;
+        background-color: #ff1111;
+    }
+        </style>
+    </head>
+    <body>
+        <div>
+            <img id="myid"/>
+            <img id='myid2'/>
+            <input name="username" placeHolders='user Name' type='text' required/>
+        </div>
+        </hr>
+    </body>
+    </html>`;
   res.end(body);
 });
 
